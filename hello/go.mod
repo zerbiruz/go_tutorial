59fcb9af8c009dad7dd196aed779c3459ae88ccf
THEERA PALAJARE <zerbiruz@gmail.com>
2022-06-07 09:21:14 +0700

    Return and handle an error

--- hello/go.mod
+++ hello/go.mod
@@ -2,8 +2,6 @@ module example/hello
 
 go 1.18
 
-require (
-	example.com/greeting v0.0.0-00010101000000-000000000000
-)
+require example.com/greeting v0.0.0-00010101000000-000000000000
 
 replace example.com/greeting => ../greeting
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greeting"
 )
 
 func main() {
-	message := greeting.Hello("Theera")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and flag to disable printing
+	// the time, source file, ane line number.
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greeting.Hello("theera")
+	// If an error was returned, print it to console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
